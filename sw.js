494a6e5d829a7162e1adb5554da37b9340938ae9
Flight-star-cy <c92258088@126.com>
2024-11-19 11:49:54 +0800

    Site updated: 2024-11-19 11:49:53

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731987949514";
+const VERSION = "1731988187129";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
